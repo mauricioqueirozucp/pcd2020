d889a6bc4082ba38067a00923eef60b59f416872
mauricioqueirozucp <mauricioqueiroz93@gmail.com>
2020-03-09 21:03:20 -0400

    Exemplo de olá mundo. - 2020 -03-09 - 22:02

---
+++ 000-ola_mundo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+## Dependencias
+#
+##
+# pip3 install ipaddress
+
+##
+# Modulos
+##
+import ipaddress
+#import sockets
+import sys
+
+#print ("Olá Mundo !!!")
+sys.stdout.write ("\nOlá Mundo \n\n")
+
+sys.stderr.write ("\n\t\tDeu ruim !!!\n")
